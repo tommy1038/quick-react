5f3cb72cfa8dda280ff6b2d9e2ee6eb755aae4ff
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-05-07 01:26:43 +0900

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
 import React from 'react';
-import Language from './Language';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends React.Component {
   render() {
-    // 指定されたコードを貼り付けてください
-    const languageList = [
-      {
-        name: 'HTML & CSS',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-      },
-      {
-        name: 'JavaScript',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-      },
-      {
-        name: 'React',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-      },
-      {
-        name: 'Ruby',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
-      },
-      {
-        name: 'Ruby on Rails',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
-      },
-      {
-        name: 'Python',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
-      }
-    ];
-
     return (
       <div>
-        <h1>言語一覧</h1>
-        <div className='language'>
-          {languageList.map((languageItem) => {
-            return (
-              <Language
-                name={languageItem.name}
-                image={languageItem.image}
-              />
-            )
-          })}
-        </div>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
