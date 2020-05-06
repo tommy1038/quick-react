03e92330d86f602a40785ee830de89781eca6c35
Ryota Tomiyama <lit.karu.noir@gmail.com>
2020-05-07 01:03:38 +0900

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'にんじゃわんこ'};
+  }
+  handleClick(name) {
+    this.setState({name: name});
+  }
+  render() {
+    return (
+    	<div>
+    	  <h1>こんにちは、{this.state.name}さん！</h1>
+        <button onClick={() => this.handleClick('ひつじ仙人')}>ひつじ仙人</button>
+        <button onClick={() => this.handleClick('にんじゃわんこ')}>にんじゃわんこ</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
